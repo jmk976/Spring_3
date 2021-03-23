d4ccae21021d2e36747bc49880d2669b2268313b
jmk976 <jmk976@gmail.com>
2021-03-23 14:31:34 +0900

    member add

--- src/main/java/com/iu/s3/member/MemberDAO.java
+++ src/main/java/com/iu/s3/member/MemberDAO.java
@@ -5,11 +5,26 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class MemberDAO {
+	
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.s3.member.MemberDAO";
+	
+	public int memberUpdate(MemberDTO memberDTO)throws Exception{
+		//id 를 제외하고 나머지를 수정  
+	}
+	
+	public int memberDelete(MemberDTO memberDTO)throws Exception{
+		
+	}
 	
 	//memberJoin 데이터를 받아서 Db에 insert하는 메서드
 	
@@ -55,43 +70,11 @@ public class MemberDAO {
 	}
 	
 	//login -id pw 를 받아서 조회
-	public MemberDTO login(MemberDTO memberDTO) throws Exception{
+	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception{
 		
 		
-		String user="user01";
-		String password="user01";
-		String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver="oracle.jdbc.driver.OracleDriver";
-	
-
 		
-		Class.forName(driver);
-		
-		Connection con = DriverManager.getConnection(url, user, password);
-		
-		String sql ="select * from member where id=? and pw=?";
-		
-		PreparedStatement st = con.prepareStatement(sql); 
-		
-		st.setString(1, memberDTO.getId());
-		st.setString(2, memberDTO.getPw());
-		
-		ResultSet rs = st.executeQuery();
-		
-		if(rs.next()) {
-			memberDTO.setName(rs.getString("name"));
-			memberDTO.setEmail(rs.getString("email"));
-			memberDTO.setPhone(rs.getString("phone"));
-		} else {
-			
-			memberDTO = null;
-		}
-		
-		rs.close();
-		st.close();
-		con.close();
-		
-		return memberDTO;
+		return sqlSession.selectOne(NAMESPACE+".memberLogin", memberDTO);
 		
 	}
 
--- src/test/java/com/iu/s3/member/MemberDAOTest.java
+++ src/test/java/com/iu/s3/member/MemberDAOTest.java
@@ -3,12 +3,20 @@ package com.iu.s3.member;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class MemberDAOTest {
+import com.iu.s3.MyAbstractTest;
 
+public class MemberDAOTest extends MyAbstractTest {
+
+	@Autowired
+	private MemberDAO memberDAO;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void memberLoginTest() throws Exception {
+		MemberDTO memberDTO
+		
+		
 	}
 
 }
