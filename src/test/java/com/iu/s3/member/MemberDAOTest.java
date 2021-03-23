ede9cd2a66a89e54d9853af610fc5520d1e611da
jmk976 <jmk976@gmail.com>
2021-03-23 14:50:47 +0900

    memberJoin add

--- src/test/java/com/iu/s3/member/MemberDAOTest.java
+++ src/test/java/com/iu/s3/member/MemberDAOTest.java
@@ -13,6 +13,20 @@ public class MemberDAOTest extends MyAbstractTest {
 	private MemberDAO memberDAO;
 	
 	@Test
+	public void memberJoinTest() throws Exception {
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId("id13");
+		memberDTO.setPw("pw13");
+		memberDTO.setName("id13");
+		memberDTO.setPhone("010-1111-1313");
+		memberDTO.setEmail("id13@naver.com");
+		
+		int result = memberDAO.memberJoin(memberDTO);
+		
+		assertEquals(1, result);
+	}
+	
+	@Test
 	public void memberLoginTest() throws Exception {
 		MemberDTO memberDTO = new MemberDTO();
 		memberDTO.setId("iu");
